20ff85b4ba87b2c11a019bb8330cdcb86a296e1b
Ornella delfino <ornella.delfino@hotmail.com>
2024-07-26 19:06:49 +0100

    added semantic html tags, components and metatags

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <header>
+        {/* <img src={logo} alt="Little Lemon Logo" /> */}
+        <h1>Welcome to My App</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+    return (
+      <main>
+        <section>
+          <p>This is the main content of the app.</p>
+        </section>
+      </main>
+    );
+  }
+  
+  export default Main;
